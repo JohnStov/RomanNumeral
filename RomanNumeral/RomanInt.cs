ecb32b8682fd8f0188804e992c491b6848bfb2ba
John Stovin <john.d.stovin@gmail.com.uk>
2013-05-08 09:09:51 +0100

    Initial commit - I

---
+++ RomanNumeral/RomanInt.cs
@@ -0,0 +1,17 @@
+﻿namespace RomanNumeral
+{
+    public class RomanInt
+    {
+        private readonly int value;
+
+        public RomanInt(int value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return "I";
+        }
+    }
+}
---
+++ RomanNumeralTest/RomanIntTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+using RomanNumeral;
+
+using Xunit;
+
+namespace RomanNumeralTest
+{
+    public class RomanIntTest
+    {
+        [Fact]
+        public void OneIsI()
+        {
+            var num = new RomanInt(1);
+            num.ToString().Should().Be("I");
+        }
+    }
+}
